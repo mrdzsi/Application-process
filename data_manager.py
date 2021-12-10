fd44d15f3b484cf5d6b1730acaa8910d1fab6d5f
Gergo <jenofigergo@gmail.com>
2021-12-10 19:29:40 +0100

    Fixed filtering

--- data_manager.py
+++ data_manager.py
@@ -1,5 +1,4 @@
 from typing import List, Dict
-
 from psycopg2 import sql
 from psycopg2.extras import RealDictCursor
 
@@ -36,5 +35,16 @@ def get_mentors_by_location(cursor, city):
         WHERE city = %(city)s ORDER BY first_name;
         """)
     params = {'city': city}
+    cursor.execute(query, params)
+    return cursor.fetchall()
+
+@database_common.connection_handler
+def get_applicant_data_by_name(cursor, first_name):
+    query = ("""
+        SELECT first_name, last_name, phone_number
+        FROM applicant
+        WHERE first_name = %(f_n)s ORDER BY first_name;
+        """)
+    params = {'f_n': first_name}
     cursor.execute(query, params)
     return cursor.fetchall()
--- main.py
+++ main.py
@@ -12,26 +12,38 @@ def index():
 
 @app.route('/mentors')
 def mentors_list():
+    # We get back a list of dictionaries from the data_manager (for details check 'data_manager.py')
+
     mentor_name = request.args.get('mentor-last-name')
+    city_name = request.args.get('city-name')
+    mentor_details = None
 
     if mentor_name:
         mentor_details = data_manager.get_mentors_by_last_name(mentor_name)
-    else:
-        mentor_details = data_manager.get_mentors()
-
-    # We get back a list of dictionaries from the data_manager (for details check 'data_manager.py')
-
-    city_name = request.args.get('city-name')
-
-    if city_name:
+    elif city_name:
         mentor_details = data_manager.get_mentors_by_location(city_name)
-    else:
-        mentor_details = data_manager.get_mentors
+    elif not mentor_name and not city_name:
+        mentor_details = data_manager.get_mentors()
 
     return render_template('mentors.html',
                            mentors=mentor_details,
                            mentor_name=mentor_name,
                            city_name=city_name)
+
+
+@app.route('/applicants-phone')
+def applicants_list():
+    applicant_first_name = request.args.get('applicant-name')
+
+    if applicant_first_name:
+        applicant_details = data_manager.get_applicant_data_by_name(applicant_first_name)
+    else:
+        redirect('/index.html')
+
+    return render_template('applicants.html',
+                           applicant_first_name=applicant_first_name)
+
+
 
 
 if __name__ == '__main__':
